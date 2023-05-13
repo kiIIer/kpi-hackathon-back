c2457f738a3b2f1064c928d96b9dda50139dfc6c
DESKTOP-MB6VI96\alosh <oleksii@office.cquelle.de>
2023-05-14 00:07:39 +0300

    #9 update routes

--- Idk/Idk.Web/Controllers/TaskController.cs
+++ Idk/Idk.Web/Controllers/TaskController.cs
@@ -2,12 +2,11 @@
 using Idk.Application.Model;
 using Idk.Application.Services.Task;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace Idk.Web.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api")]
 public class TaskController : BaseController
 {
     private readonly ITaskService _taskService;
@@ -16,35 +15,35 @@ public class TaskController : BaseController
     {
         _taskService = taskService;
     }
-    [HttpGet("{id:int}")]
-    [HttpGet("{subjectId:int}/{id:int}")]
+    [HttpGet("tasks/{id:int}")]
+    [HttpGet("subjects/{subjectId:int}/tasks/{id:int}")]
     public async Task<TaskModel> GetTaskById(int? subjectId, int id)
     {
         return await _taskService.GetTaskById(subjectId, UserId, id);
     }
 
-    [HttpGet]
+    [HttpGet("tasks")]
     public async Task<IEnumerable<TaskModel>> GetUserTasks()
     {
         return await _taskService.GetUserTasks(UserId);
     }
 
-    [HttpPost]
-    [HttpPost("{subjectId:int}")]
+    [HttpPost("tasks")]
+    [HttpPost("subjects/{subjectId:int}/tasks")]
     public async Task<TaskModel> CreateTask(TaskDto dto, int? subjectId)
     {
         return await _taskService.CreateTask(subjectId, UserId, dto);
     }
 
-    [HttpPut("{id:int}")]
-    [HttpPut("{subjectId:int}/{id:int}")]
+    [HttpPut("tasks/{id:int}")]
+    [HttpPut("subjects/{subjectId:int}/tasks/{id:int}")]
     public async Task<TaskModel> UpdateTask(int? subjectId, int id, TaskDto dto)
     {
         return await _taskService.UpdateTask(subjectId, UserId, id, dto);
     }
     
-    [HttpDelete("{id:int}")]
-    [HttpDelete("{subjectId:int}/{id:int}")]
+    [HttpDelete("tasks/{id:int}")]
+    [HttpDelete("subjects/{subjectId:int}/tasks/{id:int}")]
     public async Task DeleteTask(int? subjectId, int id)
     {
         await _taskService.DeleteTaskById(subjectId, UserId, id);
