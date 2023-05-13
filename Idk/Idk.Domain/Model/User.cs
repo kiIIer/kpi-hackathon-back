ff0bdf936dc8c8474e199ed957859545c9cb2fb4
chechmek <101751938+chechmek@users.noreply.github.com>
2023-05-13 14:42:48 +0200

    Wrote codebase for db (#3)
    * Wrote codebase for db
    
    * Added migartion
    
    * add package
    
    * downloaded ef sql server fro domain
    
    ---------
    
    Co-authored-by: roman.babukh <roman.babukh@rsrg.com>
    Co-authored-by: DESKTOP-MB6VI96\alosh <oleksii@office.cquelle.de>
    Co-authored-by: Olexiy Prokopenko <aloshaprokopenko5@gmail.com>

---
+++ Idk/Idk.Domain/Model/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Idk.Domain.Model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Nickname { get; set; }
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public ICollection<Subject> Subjects { get; set; }
+    }
+}
--- Idk/Idk.Web/Program.cs
+++ Idk/Idk.Web/Program.cs
@@ -1,3 +1,6 @@
+using Idk.Domain.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<IdkContext>((o) =>
+{
+    o.UseSqlServer(builder.Configuration.GetConnectionString("Local"));
+});
 
 var app = builder.Build();
 
