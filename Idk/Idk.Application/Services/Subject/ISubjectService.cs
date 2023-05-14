9f4d604c2cb69ae1b386cdd26b08c4d3e510b5b3
DESKTOP-MB6VI96\alosh <oleksii@office.cquelle.de>
2023-05-14 10:41:36 +0300

    add subject service

---
+++ Idk/Idk.Application/Services/Subject/ISubjectService.cs
@@ -0,0 +1,13 @@
+﻿using Idk.Application.Dtos.Subjects;
+using Idk.Application.Model;
+
+namespace Idk.Application.Services.Subject;
+
+public interface ISubjectService
+{
+    Task<SubjectModel> GetSubjectById(int id, int userId);
+    Task<IEnumerable<SubjectModel>> GetUserSubjects(int userId);
+    Task<SubjectModel> CreateSubject(SubjectDto dto, int userId);
+    Task<SubjectModel> UpdateSubject(int id, SubjectDto dto, int userId);
+    System.Threading.Tasks.Task DeleteSubjectById(int id, int userId);
+}
