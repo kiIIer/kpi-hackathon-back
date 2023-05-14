a38bdfa86930c4355677f0258f2cf61d30b9ffe4
chechmek <101751938+chechmek@users.noreply.github.com>
2023-05-14 11:37:46 +0200

    Added validation for dto models (#22)
    * Added validation for dto models
    
    * Removed validatior from service
    
    ---------
    
    Co-authored-by: roman.babukh <roman.babukh@rsrg.com>

---
+++ Idk/Idk.Application/Validation/SubjectDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Idk.Application.Dependencies;
+using Idk.Application.Dtos.Subjects;
+
+namespace Idk.Application.Validation
+{
+    public class SubjectDtoValidator : AbstractValidator<SubjectDto>
+    {
+        public SubjectDtoValidator()
+        {
+            RuleFor(subject => subject.Name)
+                .NotEmpty()
+                .MaximumLength(50);
+
+            RuleFor(subject => subject.Description)
+                .MaximumLength(1000);
+
+            RuleFor(subject => subject.MaxGrade)
+                .InclusiveBetween(0f, 100f);
+
+            RuleFor(subject => subject.Deadline)
+                .GreaterThanOrEqualTo(DateTime.UtcNow)
+                .WithMessage("Deadline must be in the future.");
+        }
+    }
+}
